e869322da2a909ffd6a80105ea64cbf355700f69
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-11-11 23:03:03 -0300

    update

--- react1/src/textDinamic/text.js
+++ react1/src/textDinamic/text.js
@@ -9,20 +9,9 @@ function TextD() {
   const frase = "Explorando o React";
   console.log("rodou");
 
-  console.log(count);
-  if (count !== frase.length) {
-    setTimeout(() => {
-      setCount(count + 1);
-    }, 2000);
-  } else {
-    setCount(0);
-    frase1 = "";
-  }
-
-  console.log(frase1);
   return (
     <div>
-      <h2>{(frase1 += frase[count])}</h2>
+      <h2>{"teste"}</h2>
     </div>
   );
 }
